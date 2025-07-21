2d3b4bf252eba1a7f76c5ecfb3a08a1952e2de2a
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2025-07-21 13:45:49 +1000

    remove output capturing in compile.sh

--- dcc-runner/compile.sh
+++ dcc-runner/compile.sh
@@ -9,42 +9,27 @@ read -r json
 echo "$json" | jq -r '.source' > $SOURCE_FILE
 echo "$json" | jq -r '.stdin' > $STDIN_FILE
 
-
-PROGRAM_OUTPUT="/tmp/program_output.txt"
-PROGRAM_ERRORS="/tmp/program_errors.txt"
-
-DCC_HELP_OUTPUT="/tmp/dcc_help_output.txt"
-COMPILE_ERRORS="/tmp/compile_errors.txt"
-
-function run_dcc_help() {
-  # do we need to catch dcc help errors?
-  if ! dcc-help > "$DCC_HELP_OUTPUT"; then
-    echo "[x] Failed to get dcc-help explanation"
-    exit 0
-  fi
-
+run_dcc_help() {
   echo "[?] Don't understand? Here's what dcc-help says:"
-  cat "$DCC_HELP_OUTPUT"
+  if ! dcc-help 2>&1; then
+    echo "[x] Failed to get dcc-help explanation"
+    exit 1
+  fi
 }
 
 # temporarily comment out dcc stuff so we can run the program instead 
 # Try to compile
-if ! dcc -Werror "$SOURCE_FILE" -o "$OUT_FILE" 2>"$COMPILE_ERRORS"; then
-  echo "[!] Compilation failed"
-  cat "$COMPILE_ERRORS"
-  run_dcc_help
+if ! dcc -Werror "$SOURCE_FILE" -o "$OUT_FILE" 2>&1; then
+#   echo "[!] Compilation failed"
+  run_dcc_help || exit 1
   exit 0
 fi
 
 # Try to run
-if ! timeout 5s "$OUT_FILE" < "$STDIN_FILE" > "$PROGRAM_OUTPUT" 2>"$PROGRAM_ERRORS"; then
-  cat $PROGRAM_OUTPUT
-  echo "[!] Runtime error or timeout"
-  cat "$PROGRAM_ERRORS"
-  run_dcc_help
+if ! "$OUT_FILE" < "$STDIN_FILE" 2>&1; then
+#   echo "[!] Runtime error or timeout"
+  run_dcc_help || exit 1
   exit 0
 fi
 
-# Print program output
-cat $PROGRAM_OUTPUT
 
