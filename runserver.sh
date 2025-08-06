83b3454a05388182c146a3da3e08a54fad759bb0
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2025-08-06 12:34:54 +1000

    move dcc-help src to git submodule

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-docker image build -t dcc-runner-dcc_help_testing dcc-runner/
+docker image build --target base -t dcc-help:base dcc-help/
+docker image build -t dcc-runner dcc-runner/
 
 npm run dev
 
